8011078a3703b7447843e8e6c13f7c485df25e26
mathetes <mathetes@gmail.com>
2018-04-03 10:02:27 +0700

    initial state with material-ui

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppBar from 'material-ui/AppBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar title="Коллекция приложений на React.js" />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+
+  return (
+    <div>
+        <p>Пустой компонент</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
